190e7bf87400f163c5f380a45b235f0adddab8e3
albanie <albanie@robots.ox.ac.uk>
2017-08-15 11:57:23 +0100

    clean up

--- example/robust_regression2.m
+++ example/robust_regression2.m
@@ -16,7 +16,7 @@ function robust_regression2(expDir, varargin)
   opts.valRatio = 0.2 ;
   opts.train.gpus = 2 ;
   opts.residualScale = 96 ;
-  opts.architecture = 'resnet50' ;
+  opts.architecture = 'simple' ;
   opts.loss = 'euclidean' ;
   opts.refreshEvaluationCache = false ;
   opts = vl_argparse(opts, varargin) ;
@@ -51,10 +51,10 @@ function robust_regression2(expDir, varargin)
   % load imdb
   imdb = loadImdb(opts) ;
 
-  [net, info] = cnn_train_autonn(net, imdb, ...
-                                   @(x,y) getBatch(x, y, opts), opts.train, ...
-                                   'val', find(imdb.images.set == 2), ...
-                                   'expDir', expDir) ;
+  cnn_train_autonn(net, imdb, ...
+                   @(x,y) getBatch(x, y, opts), opts.train, ...
+                   'val', find(imdb.images.set == 2), ...
+                   'expDir', expDir) ;
 
   bestEpoch = findBestCheckpoint(expDir, 'priorityMetric', 'regLoss', ...
                                  'prune', true) ;
@@ -81,7 +81,7 @@ if ~exist(opts.dataOpts.dataDir, 'dir')
   mkdir(opts.dataOpts.dataDir)
 end
 
-if ~exist(opts.dataOpts.imdbPath)
+if ~exist(opts.dataOpts.imdbPath, 'file')
     fprintf('downloading %s to %s\n', url, opts.dataOpts.imdbPath) ;
     websave(opts.dataOpts.imdbPath, url) ;
 end
@@ -108,10 +108,8 @@ switch opts.modelOpts.architecture
     images = repmat(images, [1 1 3 1]) ;
     images = bsxfun(@minus, images, opts.batchOpts.averageImage) ;
     images = imresize(images, opts.batchOpts.imageSize(1:2)) ;
-  case 'simple'
-    ; % do nothing
-  otherwise
-    error('architecture %s not recognised', opts.modelOpts.architecture) ;
+  case 'simple' % do nothing
+  otherwise, error('arch: %s not recognised', opts.modelOpts.architecture) ;
 end
 
 if numel(opts.train.gpus) >= 1
@@ -123,155 +121,151 @@ inputs = {'input', images, 'targets', targets} ;
 % -----------------------------------
 function [net, opts] = net_init(opts)
 % -----------------------------------
+  % the targets input will be common to all architectures
+  targets = Input('targets') ;
 
-% the targets input will be common to all architectures
-targets = Input('targets') ;
+  switch opts.modelOpts.architecture
+    case 'resnet50'
+      path = '~/data/models/matconvnet/resnet50-face-bn.mat' ;
+      dag = dagnn.DagNN.loadobj(load(path)) ;
 
-switch opts.modelOpts.architecture
-  case 'resnet50'
-
-    path = '/users/albanie/data/models/matconvnet/resnet50-face-bn.mat' ;
-    dag = dagnn.DagNN.loadobj(load(path)) ;
-
-    % remove breaking options
-    for i = 1:numel(dag.layers)
-      l = dag.layers(i) ;
-      if ismember('opts', fieldnames(l.block))
-        % remove single option blocks
-        if numel(l.block.opts) == 1
-          dag.layers(i).block.opts = {} ;
+      % remove breaking options
+      for i = 1:numel(dag.layers)
+        l = dag.layers(i) ;
+        if ismember('opts', fieldnames(l.block))
+          % remove single option blocks
+          if numel(l.block.opts) == 1
+            dag.layers(i).block.opts = {} ;
+          end
         end
       end
-    end
 
-    net = Layer.fromDagNN(dag) ;
-    last = net{1}.find(@vl_nnpool, -1) ;
-    rawPred = vl_nnconv(last, 'size', [1, 1, 2048, 30]) ;
+      net = Layer.fromDagNN(dag) ;
+      last = net{1}.find(@vl_nnpool, -1) ;
+      rawPred = vl_nnconv(last, 'size', [1, 1, 2048, 30]) ;
 
-    opts.batchOpts.imageSize = dag.meta.normalization.imageSize ;
-    averageImage = permute(dag.meta.normalization.averageImage, [3 2 1]) ;
-    opts.batchOpts.averageImage = averageImage ;
-  case 'simple'
+      opts.batchOpts.imageSize = dag.meta.normalization.imageSize ;
+      averageImage = permute(dag.meta.normalization.averageImage, [3 2 1]) ;
+      opts.batchOpts.averageImage = averageImage ;
+    case 'simple'
+      % use the name "input" for consistency with resnets
+      input = Input('input') ;
 
-    % use the name "input" for consistency with resnets
-    input = Input('input') ;
+      x = vl_nnconv(input, 'size', [3, 3, 1, 48]) ;
+      x = vl_nnbnorm(x, 'learningRate', [2 1 0.05]) ;
+      x = vl_nnrelu(x) ;
+      x = vl_nnpool(x, [2 2], 'stride', 2, 'pad', 0) ;
 
-    x = vl_nnconv(input, 'size', [3, 3, 1, 48]) ;
-    x = vl_nnbnorm(x, 'learningRate', [2 1 0.05]) ;
-    x = vl_nnrelu(x) ;
-    x = vl_nnpool(x, [2 2], 'stride', 2, 'pad', 0) ;
+      x = vl_nnconv(x, 'size', [3, 3, 48, 96]) ;
+      x = vl_nnbnorm(x, 'learningRate', [2 1 0.05]) ;
+      x = vl_nnrelu(x) ;
+      x = vl_nnpool(x, [2 2], 'stride', 2, 'pad', 0) ;
 
-    x = vl_nnconv(x, 'size', [3, 3, 48, 96]) ;
-    x = vl_nnbnorm(x, 'learningRate', [2 1 0.05]) ;
-    x = vl_nnrelu(x) ;
-    x = vl_nnpool(x, [2 2], 'stride', 2, 'pad', 0) ;
+      x = vl_nnconv(x, 'size', [3, 3, 96, 144]) ;
+      x = vl_nnbnorm(x, 'learningRate', [2 1 0.05]) ;
+      x = vl_nnrelu(x) ;
+      x = vl_nnpool(x, [2 2], 'stride', 2, 'pad', 0) ;
 
-    x = vl_nnconv(x, 'size', [3, 3, 96, 144]) ;
-    x = vl_nnbnorm(x, 'learningRate', [2 1 0.05]) ;
-    x = vl_nnrelu(x) ;
-    x = vl_nnpool(x, [2 2], 'stride', 2, 'pad', 0) ;
+      x = vl_nnconv(x, 'size', [3, 3, 144, 192]) ;
+      x = vl_nnbnorm(x, 'learningRate', [2 1 0.05]) ;
+      x = vl_nnrelu(x) ;
+      x = vl_nnpool(x, [2 2], 'stride', 2, 'pad', 0) ;
 
-    x = vl_nnconv(x, 'size', [3, 3, 144, 192]) ;
-    x = vl_nnbnorm(x, 'learningRate', [2 1 0.05]) ;
-    x = vl_nnrelu(x) ;
-    x = vl_nnpool(x, [2 2], 'stride', 2, 'pad', 0) ;
+      x = vl_nnconv(x, 'size', [4, 4, 192, 30]) ;
+      rawPred = reshape(x, 30, []) ;
 
-    x = vl_nnconv(x, 'size', [4, 4, 192, 30]) ;
-    rawPred = reshape(x, 30, []) ;
+      opts.batchOpts.imageSize = [96 96] ;
+    otherwise
+      error('architecture %s not recognised', opts.modelOpts.architecture) ;
+  end
+  opts.batchOpts.batchSize = opts.train.batchSize ;
 
-    opts.batchOpts.imageSize = [96 96] ;
-  otherwise
-    error('architecture %s not recognised', opts.modelOpts.architecture) ;
-end
-opts.batchOpts.batchSize = opts.train.batchSize ;
+  % mask out the NaNs present in the target data so that they do 
+  % not contribute to the loss, and remove the corresponding predictions
+  [pred, mTargets] = Layer.create(@vl_nnmasknan, {rawPred, targets}, 'numInputDer', 1) ;
 
-% mask out the NaNs present in the target data so that they do 
-% not contribute to the loss, and remove the corresponding predictions
-[pred, mTargets] = Layer.create(@vl_nnmasknan, {rawPred, targets}, 'numInputDer', 1) ;
+  extras = {} ;
+  switch opts.modelOpts.loss
+    case 'euclidean', loss_func = @vl_nneuclideanloss ;
+    case 'huber', loss_func = @vl_nnhuberloss ;
+    case 'tukey', extras = {'scaleRes', 1} ; loss_func = @vl_nntukeyloss ;
+    case 'tukey7', extras = {'scaleRes', 7} ; loss_func = @vl_nntukeyloss ;
+    otherwise, error('loss %s not recognised', opts.modelOpts.loss) ;
+  end
 
-extras = {} ;
-switch opts.modelOpts.loss
-  case 'euclidean', loss_func = @vl_nneuclideanloss ;
-  case 'huber', loss_func = @vl_nnhuberloss ;
-  case 'tukey', extras = {'scaleRes', 1} ; loss_func = @vl_nntukeyloss ;
-  case 'tukey7', extras = {'scaleRes', 7} ; loss_func = @vl_nntukeyloss ;
-  otherwise, error('loss %s not recognised', opts.modelOpts.loss) ;
-end
+  used = Layer.create(loss_func, [{pred, mTargets} extras], 'numInputDer', 1) ;
+  used.name = 'regLoss' ;
+  rmse = Layer.create(@vl_nnrmse, {pred, mTargets, ...
+                            'residualScale', opts.modelOpts.residualScale}, ... 
+                            'numInputDer', 0) ;
+  rmse.name = 'rmse' ;
 
-used = Layer.create(loss_func, {pred, mTargets, extras{:}}, 'numInputDer', 1) ;
-used.name = 'regLoss' ;
-rmse = Layer.create(@vl_nnrmse, {pred, mTargets, ...
-                          'residualScale', opts.modelOpts.residualScale}, ... 
-                          'numInputDer', 0) ;
-rmse.name = 'rmse' ;
+  % this is a bit of a hack so that we can easily compare the effect 
+  % of different loss functions
+  loss =  used + 0 * rmse ;
 
-% this is a bit of a hack so that we can easily compare the effect 
-% of different loss functions
-loss =  used + 0 * rmse ;
+  Layer.workspaceNames() ;
+  net = Net(loss) ;
 
-Layer.workspaceNames() ;
-net = Net(loss) ;
-
-%in = {'input', rand(224,224,3, 'single'), 'targets', rand(30, 1, 'single')} ;
-%in = {'input', rand(224,224,3, 'single')} ;
-%net.eval(in, 'forward')
+  %in = {'input', rand(224,224,3, 'single'), 'targets', rand(30, 1, 'single')} ;
+  %in = {'input', rand(224,224,3, 'single')} ;
+  %net.eval(in, 'forward')
 
 % -------------------------------------------------------------------------
 function stats = extractStats(stats, net, sel, batchSize)
 % -------------------------------------------------------------------------
-% modify statistics for accurate RMSE computation
-for i = 1:numel(sel)
-  name = net.forward(sel(i)).name ;
-  if ~isfield(stats, name)
-    stats.(name) = 0 ;
-  end
-  newValue = gather(sum(net.vars{net.forward(sel(i)).outputVar(1)}(:))) ;
+  % modify statistics for accurate RMSE computation
+  for i = 1:numel(sel)
+    name = net.forward(sel(i)).name ;
+    if ~isfield(stats, name)
+      stats.(name) = 0 ;
+    end
+    newValue = gather(sum(net.vars{net.forward(sel(i)).outputVar(1)}(:))) ;
 
-  if strcmp(name, 'rmse') % normalisation has already occured
-    iter = stats.num / batchSize ;
-    stats.(name) = ((iter-1) * stats.(name) + newValue) / iter ;
-  else
-    % Update running average (same work as dagnn.Loss)
-    stats.(name) = ((stats.num - batchSize) * stats.(name) + newValue) / stats.num ;
+    if strcmp(name, 'rmse') % normalisation has already occured
+      iter = stats.num / batchSize ;
+      stats.(name) = ((iter-1) * stats.(name) + newValue) / iter ;
+    else
+      % Update running average (same work as dagnn.Loss)
+      stats.(name) = ((stats.num - batchSize) * stats.(name) + newValue) / stats.num ;
+    end
   end
-end
-
 
 % ---------------------------
 function plotResults(results)
 % ---------------------------
 
-% set y limits to prevent matlab following anomolies
-ylims = [ 0 100 ; 0 10 ] ;
+  % set y limits to prevent matlab following anomolies
+  ylims = [ 0 100 ; 0 10 ] ;
 
-figure(1) ; clf ;
-subplot(1,2,1) ;
-hold all ;
-styles = {'o-', '+--', '+-', '.-', ':'} ;
-for i = 1:numel(results)
-  semilogy([results(i).info.val.regLoss]', styles{i}) ; 
-end
-xlabel('Training samples [x 10^3]') ; ylabel('energy') ;
-grid on ;
-h = legend(results(:).name) ;
-set(h,'color','none');
-batchSize = results(1).batchSize ;
-title(sprintf('regLoss-(bs%d)', batchSize)) ;
-ylim(ylims(1,:)) ;
-subplot(1,2,2) ;
-hold all ;
-for i = 1:numel(results)
-  plot([results(i).info.val.rmse]',styles{i}) ;
-end
-h = legend(results(:).name) ;
-grid on ;
-xlabel('Training samples [x 10^3]'); ylabel('error') ;
-set(h,'color','none') ;
-title(sprintf('rmse-(bs%d)', batchSize)) ;
-ylim(ylims(2,:)) ;
-drawnow ;
+  figure(1) ; clf ;
+  subplot(1,2,1) ;
+  hold all ;
+  styles = {'o-', '+--', '+-', '.-', ':'} ;
+  for i = 1:numel(results)
+    semilogy([results(i).info.val.regLoss]', styles{i}) ; 
+  end
+  xlabel('Training samples [x 10^3]') ; ylabel('energy') ;
+  grid on ;
+  h = legend(results(:).name) ;
+  set(h,'color','none');
+  batchSize = results(1).batchSize ;
+  title(sprintf('regLoss-(bs%d)', batchSize)) ;
+  ylim(ylims(1,:)) ;
+  subplot(1,2,2) ;
+  hold all ;
+  for i = 1:numel(results)
+    plot([results(i).info.val.rmse]',styles{i}) ;
+  end
+  h = legend(results(:).name) ;
+  grid on ;
+  xlabel('Training samples [x 10^3]'); ylabel('error') ;
+  set(h,'color','none') ;
+  title(sprintf('rmse-(bs%d)', batchSize)) ;
+  ylim(ylims(2,:)) ;
+  drawnow ;
 
-% this is a function for plotting figures in the terminal
-% (the function can be found at https://github.com/albanie/zvision)
-% but can be commented out if you are using a normal GUI
-zs_dispFig ;
+  % this is a function for plotting figures in the terminal
+  % (the function can be found at https://github.com/albanie/zvision)
+  % but can be commented out if you are using a normal GUI
+  if exist('zs_dispFig', 'file'), zs_dispFig ; end
--- matlab/vl_nntukeyloss.m
+++ matlab/vl_nntukeyloss.m
@@ -1,48 +1,47 @@
 function y = vl_nntukeyloss(x, t, varargin)
 % VL_NNTUKEYLOSS computes the Tukey Loss 
-% This is a slight modification of the robust loss function 
-% contained in the deep regression codebase 
-%   https://github.com/bazilas/matconvnet-deepReg 
-% and described in the paper:
-
-%  Robust Optimization for Deep Regression
-%  V. Belagiannis, C. Rupprecht, G. Carneiro, and N. Navab,
-%  ICCV 2015, Santiago de Chile.
-%  Copyright (C) 2016 Visual Geometry Group, University of Oxford.
-%  All rights reserved.
+%    This is a slight modification of the robust loss function 
+%    contained in the deep regression codebase 
+%    https://github.com/bazilas/matconvnet-deepReg 
+%    and described in the paper:
 %
-% This file is made available under the terms of the BSD license
-% (see the COPYING file).
+%    Robust Optimization for Deep Regression
+%    V. Belagiannis, C. Rupprecht, G. Carneiro, and N. Navab,
+%    ICCV 2015, Santiago de Chile.
+%    Copyright (C) 2016 Visual Geometry Group, University of Oxford.
 %
 % (modified by Samuel Albanie, 2017)
 
-opts.scaleRes = 1 ;
-opts.instanceWeights = ones(size(x)) ;
-[opts, dzdy] = vl_argparsepos(opts, varargin, 'nonrecursive') ;
+  opts.scaleRes = 1 ;
+  opts.adaptInliers = 0 ;
+  opts.instanceWeights = ones(size(x)) ;
+  [opts, dzdy] = vl_argparsepos(opts, varargin, 'nonrecursive') ;
 
-% residuals
-res = x - t ;
+  % residuals
+  res = x - t ;
 
-% Median absolute deviation (MAD)
-MAD = 1.4826 * mad(res, 1) ;
-C = 4.685 ;
+  % Median absolute deviation (MAD)
+  MAD = 1.4826 * mad(res, 1) ;
+  C = 4.685 ;
 
-% inliers (percentage of inliers)
-nonZer = round(100 * sum(abs(res(:)) < C) / numel(res)) ;
+  % inliers (percentage of inliers)
+  nonZer = round(100 * sum(abs(res(:)) < C) / numel(res)) ;
 
-% Big V says that this sometimes helps the convergence
-MAD = MAD * opts.scaleRes ; 
+  % Big V says that this sometimes helps the convergence
+  if opts.adaptInliers % as in the paper
+    if nonZer < 70, MAD = MAD * opts.scaleRes ; end
+  end
 
-res = bsxfun(@rdivide, res, MAD) ;
+  res = bsxfun(@rdivide, res, MAD) ;
 
-if isempty(dzdy) 
+  if isempty(dzdy) 
     scale = (C^2) / 6 ;
     yt = scale * (1 - (1 - (res ./ C).^2).^3) ;
-    yt(find(abs(res) > C)) = scale ;
+    yt(abs(res) > C) = scale ;
     y = opts.instanceWeights(:)' * yt(:) ;
-else
+  else
     keep = boolean(abs(res) < C) ;
     tukDer = res .* ((1 - (res ./ C).^2).^2) ;
     res = tukDer * dzdy{1} .* keep .* opts.instanceWeights ;
     y = bsxfun(@rdivide, res, MAD) ;
-end
+  end
